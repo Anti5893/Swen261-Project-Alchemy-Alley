e68433a3cf8f5556f6263bd3053d92e5db893333
Joseph <joeys0527@gmail.com>
2024-02-29 14:18:35 -0500

    logout button removes local storage

--- estore-ui/src/app/app.component.ts
+++ estore-ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CredentialsService } from './credentials.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: any;
+
+  constructor(private credentialsService : CredentialsService){ }
+
+  logOut(){
+    this.credentialsService.removeCurrentUser()
+  }
 }
