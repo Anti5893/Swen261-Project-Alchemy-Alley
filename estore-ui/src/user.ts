f69039294a5bc93d7006a715cf311f8053983079
Joseph <joeys0527@gmail.com>
2024-03-04 15:58:51 -0500

    user:unlocked and user:cart are no longer optional

--- estore-ui/src/user.ts
+++ estore-ui/src/user.ts
@@ -2,6 +2,6 @@ export interface User{
     username: string
     password: string,
     isAdmin : boolean,
-    unlocked ? : number[],
-    cart ? : number[]
+    unlocked  : number[],
+    cart  : number[]
 }
