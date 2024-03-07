f5d23fd9df6f434e365d7e7a2075dac71c7d0c9c
Garrett59100 <garrett59100@gmail.com>
2024-03-07 17:33:07 -0500

    Added the no products found thingamabober 😎 ‼️

--- estore-ui/src/app/catalog/catalog.component.ts
+++ estore-ui/src/app/catalog/catalog.component.ts
@@ -1,26 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../product.service';
-import { Product } from '../product';
+import { Component, OnInit } from "@angular/core";
+
+import { ProductService } from "../product.service";
+import { Product } from "../product";
+import { Observable, catchError } from "rxjs";
+import { HttpErrorResponse } from "@angular/common/http";
 
 @Component({
-	selector: 'app-catalog',
-	templateUrl: './catalog.component.html',
-	styleUrl: './catalog.component.css',
+	selector: "app-catalog",
+	templateUrl: "./catalog.component.html",
+	styleUrl: "./catalog.component.css",
 })
 export class CatalogComponent implements OnInit {
 	products: Product[] = [];
+	searchQuery: string = "";
 
 	constructor(private productService: ProductService) {}
 
 	ngOnInit(): void {
-		console.log('catalog initializing...');
 		this.getProducts();
-		console.log('catalog initialized');
 	}
 
 	getProducts(): void {
-		this.productService
-			.getProducts()
-			.subscribe((products) => (this.products = products));
+		this.productService.getProducts().subscribe((products) => {
+			if (products.body) {
+				this.products = products.body;
+			} else {
+				this.products = [];
+			}
+		});
+	}
+
+	searchProducts(query: string): void {
+		this.productService.searchProduct(query).subscribe(
+			(response) => {
+				if (!response.ok) {
+					console.log("status was not ok");
+					this.products = [];
+				} else if (response.body) {
+					this.products = response.body;
+				}
+			},
+			(error) => {
+				this.products = [];
+			}
+		);
 	}
 }
--- estore-ui/src/app/product.service.ts
+++ estore-ui/src/app/product.service.ts
@@ -1,54 +1,45 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, catchError, of, tap } from 'rxjs';
-import { Product } from './product';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpResponse, HttpHeaders } from "@angular/common/http";
+import { Observable, of } from "rxjs";
+
+import { Product } from "./product";
 
 @Injectable({
-	providedIn: 'root',
+	providedIn: "root",
 })
 export class ProductService {
-	private productsUrl = 'http://localhost:8080/inventory/products';
+	private productsUrl = "http://localhost:8080/inventory/products";
 
 	httpOptions = {
-		headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+		headers: new HttpHeaders({ "Content-Type": "application/json" }),
+		observe: "response" as "response",
 	};
 
 	constructor(private http: HttpClient) {}
 
-	getProducts(): Observable<Product[]> {
-		return this.http.get<Product[]>(`${this.productsUrl}/`);
+	getProducts(): Observable<HttpResponse<Product[]>> {
+		return this.http.get<Product[]>(`${this.productsUrl}/`, this.httpOptions);
 	}
 
-	getProduct(id: number): Observable<Product> {
+	getProduct(id: number): Observable<HttpResponse<Product>> {
 		const url = `${this.productsUrl}/${id}`;
-		return this.http.get<Product>(url);
+		return this.http.get<Product>(url, this.httpOptions);
 	}
 
-	searchProduct(name: string): Observable<Product[]> {
-		if (!name.trim) {
-			return of([]);
-		}
+	searchProduct(name: string): Observable<HttpResponse<Product[]>> {
 		const url = `${this.productsUrl}?name=${name}`;
-		return this.http.get<Product[]>(url);
+		return this.http.get<Product[]>(url, this.httpOptions);
 	}
 
-	addProduct(product: Product): Observable<Product> {
-		return this.http.post<Product>(
-			`${this.productsUrl}/`,
-			product,
-			this.httpOptions
-		);
+	addProduct(product: Product): Observable<HttpResponse<Product>> {
+		return this.http.post<Product>(this.productsUrl, product, this.httpOptions);
 	}
 
-	updateProduct(product: Product): Observable<Product> {
-		return this.http.put<Product>(
-			`${this.productsUrl}/`,
-			product,
-			this.httpOptions
-		);
+	updateProduct(product: Product): Observable<HttpResponse<Product>> {
+		return this.http.put<Product>(this.productsUrl, product, this.httpOptions);
 	}
 
-	deleteProduct(id: number): Observable<Product> {
+	deleteProduct(id: number): Observable<HttpResponse<Product>> {
 		const url = `${this.productsUrl}/${id}`;
 		return this.http.delete<Product>(url, this.httpOptions);
 	}
