b6b01bdf6e7b15cfd3d61efb1f46bfd2f7a1aaf9
Shawn Broderick <spb8026@rit.edu>
2024-03-10 00:22:14 -0500

    Add and Removal to Cart working as well as part of the cart page being complete

--- estore-ui/src/app/catalog/catalog.component.ts
+++ estore-ui/src/app/catalog/catalog.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 
 import { ProductService } from "../product.service";
 import { Product } from "../product";
+import { CredentialsService } from "../credentials.service";
+import { UserService } from './../user.service';
 
 @Component({
 	selector: "app-catalog",
@@ -12,7 +14,11 @@ export class CatalogComponent implements OnInit {
 	products: Product[] = [];
 	searchQuery: string = "";
 
-	constructor(private productService: ProductService) {}
+	constructor(
+		private productService: ProductService,
+		private credentialService: CredentialsService,
+		private userService: UserService
+		) {}
 
 	ngOnInit(): void {
 		this.getProducts();
@@ -40,4 +46,50 @@ export class CatalogComponent implements OnInit {
 			}
 		);
 	}
+
+	itemInCart(product: Product): boolean {
+		const curUser = this.credentialService.getUser();
+		const curCart = curUser?.cart;
+		return Boolean(curCart?.some((productId: number) => productId == product.id));
+	}
+
+	maxCartSize():boolean {
+		const curUser = this.credentialService.getUser();
+		let curCart = curUser?.cart;
+		if (curCart)
+		{
+			return (curCart.length >= 2);
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	addToCart(product: Product): void {
+		let curUser = this.credentialService.getUser();
+		if (curUser) {
+			if (!curUser.cart) {
+				curUser.cart = [];
+			}
+	
+			curUser.cart.push(product.id);
+	
+			this.credentialService.storeCurrentUser({...curUser});
+			this.userService.updateUser(curUser).subscribe();
+		} 
+	}
+
+	removeFromCart(productID: number): void {
+		let curUser = this.credentialService.getUser();
+	 
+		 if (curUser && curUser.cart) {
+	 
+			 curUser.cart = curUser.cart.filter(id => id !== productID);
+			 this.credentialService.storeCurrentUser({...curUser});
+			
+			 this.userService.updateUser(curUser).subscribe({});
+		 }
+	 }
+	
 }
