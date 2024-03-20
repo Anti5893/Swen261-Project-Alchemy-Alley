77d0b6d07b27ae629fdf58dccbc1ae6a68cd8839
JackyFTW <jackmbarter@gmail.com>
2024-03-20 16:49:19 -0400

    Fixed validation errors for inputs with stricter requirements

--- estore-ui/src/app/admin/admin.component.ts
+++ estore-ui/src/app/admin/admin.component.ts
@@ -25,17 +25,41 @@ export class AdminComponent implements OnInit {
     this.productService.getProducts().subscribe(response => this.products = response.body!);
   }
 
-  save(product: Product, name: string, type: string, price: HTMLInputElement, quantity: HTMLInputElement): void {
-    if(!price.checkValidity() || !quantity.checkValidity()) {
+  create(name: string, type: string, price: HTMLInputElement, quantity: HTMLInputElement): void {
+    // Validate input boxes
+    if(name.trim().length == 0 || !price.checkValidity() || !quantity.checkValidity()) {
       this.showValidityError = true;
       return;
     }
 
+    // Create product instance
+    let product: Product = {
+      id: 0,
+      name: name,
+      type: Object.keys(ElementType).filter(k => k === type)[0] as ElementType,
+      price: Number(price.value),
+      quantity: Number(quantity.value)
+    };
+
+    // Send request
+    this.productService.addProduct(product).subscribe(p => this.products.push(p.body!)); // TODO: Handle duplicate names
+    this.showValidityError = false;
+  }
+
+  save(product: Product, name: string, type: string, price: HTMLInputElement, quantity: HTMLInputElement): void {
+    // Validate input boxes
+    if(name.trim().length == 0 || !price.checkValidity() || !quantity.checkValidity()) {
+      this.showValidityError = true;
+      return;
+    }
+
+    // Update product fields
     product.name = name;
     product.type = Object.keys(ElementType).filter(k => k === type)[0] as ElementType;
     product.price = Number(price.value);
     product.quantity = Number(quantity.value);
     
+    // Send request
     this.productService.updateProduct(product).subscribe();
     this.showValidityError = false;
   }
@@ -45,22 +69,10 @@ export class AdminComponent implements OnInit {
     this.productService.deleteProduct(id).subscribe();
   }
 
-  create(name: string, type: string, price: HTMLInputElement, quantity: HTMLInputElement): void {
-    if(!price.checkValidity() || !quantity.checkValidity()) {
-      this.showValidityError = true;
-      return;
-    }
-
-    let product: Product = {
-      id: 0,
-      name: name,
-      type: Object.keys(ElementType).filter(k => k === type)[0] as ElementType,
-      price: Number(price.value),
-      quantity: Number(quantity.value)
-    };
-
-    this.productService.addProduct(product).subscribe(p => this.products.push(p.body!)); // TODO: Handle duplicate names
-    this.showValidityError = false;
+  hasChanged(product: Product, name: string, type: string, price: HTMLInputElement, quantity: HTMLInputElement): boolean {
+    // Check if any field has changed at all
+    return product.name !== name || product.type !== type || 
+           product.price !== Number(price.value) || product.quantity !== Number(quantity.value);
   }
   
 }
