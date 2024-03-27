66c2280c543d90ddf145f15c04944179f2b471d7
Garrett59100 <garrett59100@gmail.com>
2024-03-27 10:37:32 -0400

    Updated the login page to allow pressing `enter` to submit

--- estore-ui/src/app/login/login.component.ts
+++ estore-ui/src/app/login/login.component.ts
@@ -1,55 +1,57 @@
-import { Component } from '@angular/core';
-import { Router} from '@angular/router';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
-import { User } from '../user';
-import { UserService } from '../user.service';
-import { CredentialsService } from '../credentials.service';
+import { User } from "../user";
+import { UserService } from "../user.service";
+import { CredentialsService } from "../credentials.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+	selector: "app-login",
+	templateUrl: "./login.component.html",
+	styleUrl: "./login.component.css",
 })
 export class LoginComponent {
+	usernameValue: string = "";
+	passwordValue: string = "";
+	users: User[] = [];
+	isAuthenticated = false;
+	requestSent = false;
 
-  username: string = '';
-  password: string = '';
-  users: User[] = [];
-  isAuthenticated = false;
-  requestSent = false;
+	constructor(
+		private userService: UserService,
+		private credentialsService: CredentialsService,
+		private router: Router
+	) {}
 
-  constructor(private userService: UserService, private credentialsService: CredentialsService, private router: Router) {}
+	fieldsFull(): boolean {
+		return this.passwordValue != "" && this.usernameValue != "";
+	}
 
-  fieldsFull(): boolean {
-    return this.password != '' && this.username != '';
-  }
- 
-  showErrorMesssage(): boolean {
-    return this.requestSent && !this.isAuthenticated;
-  }
+	showErrorMesssage(): boolean {
+		return this.requestSent && !this.isAuthenticated;
+	}
 
-  onClick(username: string, password: string): void {
-    this.userService.authenticateUser({username,password} as User).subscribe(
-      (response) => {
-        if(response.status == 200) {
-          this.isAuthenticated = true;
-          const loggedUser = response.body!;
-          this.credentialsService.storeCurrentUser(loggedUser);
-          if(loggedUser.admin) {
-            this.router.navigate(['/admin']);
-          } else {
-            this.router.navigate(['/catalog']);
-          }
-        }
-        this.requestSent = true;
-      },
-      (error) => {
-        if(this.fieldsFull()) {
-          this.isAuthenticated = false;
-        }
-        this.requestSent = true;
-      }
-    );
-  }
-  
+	onClick(username: string, password: string): void {
+		this.userService.authenticateUser({ username, password } as User).subscribe(
+			(response) => {
+				if (response.status == 200) {
+					this.isAuthenticated = true;
+					const loggedUser = response.body!;
+					this.credentialsService.storeCurrentUser(loggedUser);
+					if (loggedUser.admin) {
+						this.router.navigate(["/admin"]);
+					} else {
+						this.router.navigate(["/catalog"]);
+					}
+				}
+				this.requestSent = true;
+			},
+			(error) => {
+				if (this.fieldsFull()) {
+					this.isAuthenticated = false;
+				}
+				this.requestSent = true;
+			}
+		);
+	}
 }
